0a319a4bc8337c7c1980dc9a7ff0223dece1dd65
Rafael Teodosio <rafzteodosio@gmail.com>
2019-11-12 14:30:45 -0300

    Configurando Rota

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import SignIn from './pages/SignIn/SignIn'
 import SignUp from './pages/SignUp/SignUp'
 import RegisterSkills from './pages/RegisterSkills/RegisterSkills'
+import ListConfirmed from  './pages/ListConfirmed/ListConfirmed'
 
 const Routes = createAppContainer(
     createSwitchNavigator({
         SignIn,
         SignUp,
         RegisterSkills,
+        ListConfirmed,
     })
 );
 
