938f6afe8eb3ffede30b9b774ca27dc89dbd4ea5
Rafael Teodosio <rafzteodosio@gmail.com>
2019-11-14 00:14:21 -0300

    Adicionando Modal de sortear times

--- src/pages/ListConfirmed/ListConfirmed.js
+++ src/pages/ListConfirmed/ListConfirmed.js
@@ -11,6 +11,7 @@ export default function ListConfirmed() {
   const [DATA, setData] = useState(undefined);
   const [isLoading, setLoading] = useState(true);
   const [isVisible, setVisible] = useState(false);
+  const [isRandom, setRandom] = useState(false);
   const [peladaDate,setPeladaDate] = useState({dia:'XX/XX/XXXX',hora:'XX:XX'});
   
 useEffect(()=> {
@@ -86,9 +87,67 @@ if(!isLoading){
           />
           </View>
       </Overlay>
+
+      <Overlay  
+        onBackdropPress={()=>{setRandom(false)}} 
+        isVisible={isRandom} 
+        windowBackgroundColor="rgba(255, 255, 255, .5)" 
+        overlayBackgroundColor="#10A971"
+        width={'70%'}
+        height={'25%'}
+        overlayStyle={{
+          borderRadius: 30,shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 12,
+          },
+          shadowOpacity: 0.58,
+          shadowRadius: 16.00,
+          elevation: 24,
+          alignItems:'center',
+        }}
+      >
+          <Text h4 style={{color:'white'}}>Sortear Times ?</Text>
+          <View style={{flexDirection:'row',marginTop:50,justifyContent:'space-between'}}>
+          <Button
+            buttonStyle={{
+              backgroundColor:'red', 
+              width:110, 
+              borderRadius:10,
+              shadowOffset: {
+              width: 0,
+              height: 12,
+            },
+              shadowOpacity: 0.58,
+              shadowRadius: 16.00,
+              elevation: 5,
+            }}
+            title='RECUSAR'
+            onPress={()=>setRandom(false)}
+          />
+          <Button
+            buttonStyle={{
+              shadowOffset: {
+              width: 0,
+              height: 12,
+            },
+              shadowOpacity: 0.58,
+              shadowRadius: 16.00,
+              elevation: 5,
+              backgroundColor:'#00FF9F', 
+              marginLeft:20,
+              marginRight:0, 
+              borderRadius:10
+            }}
+            title='CONFIRMAR'
+            onPress={()=>setRandom(false)}
+          />
+          </View>
+      </Overlay>
+
       <View style={{backgroundColor:'#FFF',flexDirection:'row',justifyContent:'space-between', height:'12%', alignItems:'center'}}>
         <View style={{marginLeft:20, marginRight:20,width:'38%',marginBottom:30}}>
-          <SmallButtons text='Sortear times'/>
+          <SmallButtons onPress={()=>{setRandom(true)}} text='Sortear times'/>
         </View>
         <View style={{width:'38%',marginBottom:30,marginLeft:25, marginRight:25}}>
           <SmallButtons text='Convidar'/>
