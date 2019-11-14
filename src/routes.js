76fd9c834b76a066a666c5cdb639cb7b34cfbbc4
byronkamal <byronkamal@hotmail.com>
2019-11-14 02:18:55 -0300

    Adicionando Top Tab Navigator

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,50 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+/* eslint-disable react/react-in-jsx-scope */
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  Header,
+} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
+import React from 'react';
+import {View, Platform} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import RegisterSkills from './pages/RegisterSkills/RegisterSkills';
 import Home from './pages/Home';
 
 const Routes = createAppContainer(
-  createSwitchNavigator({
-    App: createBottomTabNavigator(
-      {
-        Home,
-      },
-      {
-        tabBarOptions: {
-          activeTintColor: '#000',
-          style: {
-            backgroundColor: '#FFF',
-          },
+  createMaterialTopTabNavigator(
+    {
+      SignIn: {
+        screen: SignIn,
+        navigationOptions: {
+          headerTitle: 'SignIn',
         },
       },
-      {
-        initialRouteName: 'App',
-      }
-    ),
-  })
+      SignUp: {
+        screen: SignUp,
+        navigationOptions: {
+          headerTitle: 'SignUp',
+        },
+      },
+    },
+    {
+      tabBarOptions: {
+        labelStyle: {
+          fontSize: 20,
+          color: 'black',
+        },
+        tabStyle: {
+          height: 70,
+        },
+        style: {
+          backgroundColor: 'white',
+        },
+      },
+    }
+  )
 );
 
 export default Routes;
