1c3011535cdb9763da4ca092e20e429f51cc167d
byronkamal <byronkamal@hotmail.com>
2019-11-14 02:53:48 -0300

    Configurando Top Tab Navigator corretamente

--- src/routes.js
+++ src/routes.js
@@ -1,48 +1,55 @@
 /* eslint-disable react/react-in-jsx-scope */
-import {
-  createAppContainer,
-  createSwitchNavigator,
-  Header,
-} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
-import React from 'react';
-import {View, Platform} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import RegisterSkills from './pages/RegisterSkills/RegisterSkills';
 import Home from './pages/Home';
 
+// deve ser verificado este valor para saber ser o usuario está ou não "Logado" e depois renderizar a respectiva tela
+const signedIn = true;
+
 const Routes = createAppContainer(
-  createMaterialTopTabNavigator(
+  createSwitchNavigator(
     {
-      SignIn: {
-        screen: SignIn,
-        navigationOptions: {
-          headerTitle: 'SignIn',
+      Sign: createSwitchNavigator({
+        SignIn,
+        SignUp,
+        RegisterSkills,
+      }),
+      App: createMaterialTopTabNavigator(
+        {
+          Home: {
+            screen: Home,
+            navigationOptions: {
+              headerTitle: 'SignIn',
+            },
+          },
+          SignIn: {
+            screen: Home,
+            navigationOptions: {
+              headerTitle: 'SignUp',
+            },
+          },
         },
-      },
-      SignUp: {
-        screen: SignUp,
-        navigationOptions: {
-          headerTitle: 'SignUp',
-        },
-      },
+        {
+          tabBarOptions: {
+            labelStyle: {
+              fontSize: 20,
+              color: 'black',
+            },
+            tabStyle: {
+              height: 70,
+            },
+            style: {
+              backgroundColor: 'white',
+            },
+          },
+        }
+      ),
     },
     {
-      tabBarOptions: {
-        labelStyle: {
-          fontSize: 20,
-          color: 'black',
-        },
-        tabStyle: {
-          height: 70,
-        },
-        style: {
-          backgroundColor: 'white',
-        },
-      },
+      initialRouteName: signedIn ? 'App' : 'Sign',
     }
   )
 );
