33f18ea7b2c4143489b53f3fbdd46d3f8ac42680
byronkamal <byronkamal@hotmail.com>
2019-11-16 01:24:58 -0300

    Melhorando layout da tela Home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,11 +1,41 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background: #ffffff;
   padding: 20px;
 `;
 
+export const SoccerGroupInfo = styled.View`
+  justify-content: center;
+`;
+
+export const SoccerImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Title = styled.Text`
+  font-family: Roboto;
+  font-weight: 700;
+  font-size: 25px;
+  color: #fff;
+  margin-top: 365px;
+  margin-horizontal: 105px;
+`;
+
+export const NoGroupText = styled.Text`
+  font-family: Roboto;
+  font-weight: 700;
+  font-size: 45px;
+  color: #9499a1;
+  text-align: center;
+`;
+
+export const GroupButton = styled.TouchableOpacity`
+`;
+
+// Navigation Options
 export const GroupIcon = styled.Image`
   margin-top: 10px;
   height: 50px;
@@ -14,7 +44,7 @@ export const GroupIcon = styled.Image`
 `;
 
 export const TabBarLabel = styled.Text`
-  font-family: Robot;
+  font-family: Roboto;
   font-weight: 700;
   font-size: 17px;
   color: #000;
