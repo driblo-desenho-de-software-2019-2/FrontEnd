d748cfddc04798cdbe1f2de8d1eccbb545f49429
Caio Oliveira de Moraes <caiooliv97@gmail.com>
2019-11-18 03:16:02 -0300

    Criada área de informação das peladas

---
+++ src/pages/Game/GameStats.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import { Container,Title,InformationTitle, InformationsView, Informations} from './styles';
+import CardView from 'react-native-cardview'
+
+export default function GameStats() {
+
+  const [place, setPlace] = useState('Local')
+  const [date, setDate] = useState('XX/XX/XX')
+  const [time, setTime] = useState('XX:XX')
+
+  return (
+    <Container>
+      <Title>Informações da pelada</Title>
+      <CardView
+        style={{marginTop:10}}
+        cardElevation={2}
+        cardMaxElevation={2}
+        cornerRadius={5}>
+
+        <InformationsView>
+          <InformationTitle>Local: </InformationTitle>
+          <Informations>{place}</Informations>
+        </InformationsView>
+        <InformationsView>
+          <InformationTitle>Data: </InformationTitle>
+          <Informations>{date}</Informations>
+        </InformationsView>
+        <InformationsView>
+          <InformationTitle>Hora: </InformationTitle>
+          <Informations>{time}</Informations>
+        </InformationsView>
+      </CardView>
+    </Container>
+  );
+}
---
+++ src/pages/Game/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex:1;
+  background-color: #f5f5f5;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  margin-top:20px;
+  font-size:25;
+  color: #7e7e7e
+
+`;
+export const InformationTitle = styled.Text`
+  font-size:17;
+  font-weight:bold;
+  margin-left:10;
+  margin-bottom:10;
+
+`;
+
+export const InformationsView = styled.View`
+  flex-direction: row;
+
+  width:100%;
+  flex-wrap: wrap;
+  margin-top:10;
+`;
+
+export const Informations = styled.Text`
+  font-size:15;
+  margin-left: 60%;
+
+
+`;
+// View style={{marginTop:20,alignItems:'flex-start', width:'80%',backgroundColor:'#fff'}}>
