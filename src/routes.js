e80f59b6618ff0ce4494d50f0232f922d4a9c36d
byronkamal <byronkamal@hotmail.com>
2019-11-17 21:04:46 -0300

    Arrumando rotas

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,12 @@ import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import RegisterSkills from './pages/RegisterSkills/RegisterSkills';
-import ListConfirmed from  './pages/ListConfirmed/ListConfirmed'
-import Profile from './pages/Profile/Profile'
+import ListConfirmed from './pages/ListConfirmed/ListConfirmed';
+import Profile from './pages/Profile/Profile';
 import Home from './pages/Home';
 
 // deve ser verificado este valor para saber ser o usuario está ou não "Logado" e depois renderizar a respectiva tela
-const signedIn = true;
+const signedIn = false;
 
 const Routes = createAppContainer(
   createSwitchNavigator(
